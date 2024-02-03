a2a11fd8a831dca60927e32656149d3bc71b4d0e
Yipeng Hu <yipeng.hu@ucl.ac.uk>
2024-02-03 13:36:51 +0000

    wip #11 voxel points extracted

--- region_correspondence/optim.py
+++ region_correspondence/optim.py
@@ -9,18 +9,13 @@ def iterative_ddf(mov, fix, control_grid_size=None, device=None, max_iter=int(1e
     '''
     Implements the free-form deformation (FFD) estimation based on control point grid (control_grid), using iterative optimisation
         when control_grid_size = None, the dense displacement field (DDF) estimation is estimated using the iterative optimisation
-    mov: torch.tensor of shape (C,D0,H0,W0) where C is the number of masks
-    fix: torch.tensor of shape (C,D1,H1,W1) where C is the number of masks
+    mov: torch.tensor of shape (C,D0,H0,W0) for 3d, where C is the number of masks, (C,H0,W0) for 2d
+    fix: torch.tensor of shape (C,D1,H1,W1) for 3d, where C is the number of masks, (C,H1,W1) for 2d
     control_grid_size: 
         None for DDF estimation
         when specified, tuple of 3 ints for 3d, tuple of 2 ints for 2d, or tuple of 1 int for the same size in all dimensions
     Returns a dense displacement field (DDF) of shape (D1,H1,W1,3) where the dim=3 contains the displacement vectors
     '''
-    num_masks = mov.shape[0]
-    if num_masks != fix.shape[0]:
-        raise ValueError("mov and fix must have the same number of masks.")
-    if mov.dim() != fix.dim():
-        raise ValueError("mov and fix must have the same dimensionality.")
     if isinstance(control_grid_size,int):
         if mov.dim() == 4:
             control_grid_size = (control_grid_size,control_grid_size,control_grid_size)
@@ -71,3 +66,29 @@ def iterative_ddf(mov, fix, control_grid_size=None, device=None, max_iter=int(1e
         optimizer.step()
     
     return ddf, control_grid 
+
+
+def ls_transform(mov, fix, transform_type='affine', device=None):
+    '''
+    Implements the least-squares (LS) estimation of parametric transformation 
+    mov: torch.tensor of shape (C,D0,H0,W0) for 3d, where C is the number of masks, (C,H0,W0) for 2d
+    fix: torch.tensor of shape (C,D1,H1,W1) for 3d, where C is the number of masks, (C,H1,W1) for 2d
+    transform_type: str, one of ["affine", "rigid", "rigid7"]
+
+    '''
+    ## extract voxel/pixel coordinates
+    fix_grid = get_reference_grid(grid_size=fix.shape[1:], device=device)
+    mov_grid = get_reference_grid(grid_size=mov.shape[1:], device=device)
+    
+    mask_indexing = lambda grid, masks: [torch.stack([grid[...,d][masks[n,...]] for d in range(grid.shape[-1])],dim=-1) for n in range(masks.shape[0])]
+    fix_coords = mask_indexing(fix_grid, fix)
+    mov_coords = mask_indexing(mov_grid, mov)
+
+    affine_matrix = gaussian_alignment(mov_coords, fix_coords, transform_type)
+
+    ddf = fix_grid @ affine_matrix  - fix_grid
+
+    ddf=None
+    affine_matrix=None
+    return ddf, affine_matrix
+    
--- region_correspondence/paired_regions.py
+++ region_correspondence/paired_regions.py
@@ -1,7 +1,7 @@
 
 import torch
 
-from region_correspondence.optim import iterative_ddf
+from region_correspondence.optim import iterative_ddf, ls_transform
 
 
 class PairedRegions():
@@ -13,12 +13,20 @@ class PairedRegions():
         masks_fix: torch.tensor of shape (C,D1,H1,W1) for 3d, 
                             (C,H1,W1) for 2d, where C is the number of masks
         '''
+        self.check_input(masks_mov, masks_fix)
         self.masks_mov = masks_mov
         self.masks_fix = masks_fix
         self.device = device
         if self.device is not None:
             self.masks_mov = self.masks_mov.to(device)
             self.masks_fix = self.masks_fix.to(device)
+    
+    def check_input(self, mov, fix):
+        if mov.shape[0] != fix.shape[0]:
+            raise ValueError("mov and fix must have the same number of masks.")
+        if mov.dim() != fix.dim():
+            raise ValueError("mov and fix must have the same dimensionality.")
+
 
     def get_dense_correspondence(self, transform_type='ddf', **kwargs):
         '''
@@ -33,7 +41,9 @@ class PairedRegions():
             case 'ffd':
                 self.ddf, self.control_grid = iterative_ddf(mov=self.masks_mov.type(torch.float32), fix=self.masks_fix.type(torch.float32), control_grid_size=10, device=self.device, **kwargs) 
             case 'affine':
-                raise NotImplementedError("TPS transform is not implemented yet.")
+                self.ddf, self.affine_matrix = ls_transform(mov=self.masks_mov, fix=self.masks_fix, transform_type='affine', device=self.device, **kwargs)
+            case 'rigid':
+                self.ddf, self.affine_matrix = ls_transform(mov=self.masks_mov, fix=self.masks_fix, transform_type='rigid', device=self.device, **kwargs)
             case 'spline':
                 raise NotImplementedError("TPS transform is not implemented yet.")
             case _:
